bd1d6106301f68f4b5f4e2a18bf79dbb640b21d0
szymek <molinski.szymon@gmail.com>
2016-05-12 22:20:21 +0200

    Geolocation / Sosnowsky's Hogweed / style.css for descriptions

--- js/init.js
+++ js/init.js
@@ -4,9 +4,9 @@
 		doubleClickZoom: false,
 		zoomControl: false
 	});							//tworzenie containera na mapę
-			
+
 map.createPane('labels');				//"warstwa" (nie mylić z layer) na mapie znajdująca się powyżej innych
-map.getPane('labels').style.zIndex = 650;	// wyświetlająca labelsy
+map.getPane('labels').style.zIndex = 550;	// wyświetlająca labelsy
 map.getPane('labels').style.pointerEvents = 'none'; 	//i nie reagująca na eventy myszki
 
 var teren = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png',{
@@ -15,14 +15,14 @@ var teren = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}
 
 
 var labels = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png',{
-	pane:'labels'  
+	pane:'labels'
 }).addTo(map);		//warstwa nazw, wyświetlana w 'pane' stworzonym wcześniej
 
 L.control.scale({position: 'bottomright'}).addTo(map);	//ręcznie dodane kontrolki zoomu w prawym rogu (domyślnie w lewym)
 
 document.getElementById("data").innerHTML = '<b>choose data to show</b>';	//wyświetlany tekst w oknie z danymi po prawej
 
-		
+
 var group = new L.featureGroup();	//wszystkie aktualnie wyświetlane geojsony
 
 $("document").ready(function() {
@@ -32,22 +32,81 @@ $("document").ready(function() {
       }
   });
 });
-			
 
+var marker = null;
+var locError;
+
+$('#locate').on('change', function(){
+
+		if (this.checked){
+			map.locate({setView: false, watch:true, maxZoom: 10});
+			setTimeout(function(){
+				if (typeof locError != "undefined"){
+					$('#locatelabel').tooltip('show');
+					setTimeout(function(){
+						$('#locatelabel').tooltip('hide');
+					}, 4000);
+				}		
+			}, 50);
+		}
+		else {
+			map.stopLocate();
+			if (marker != null){
+				map.removeLayer(marker);
+				marker = null;
+			}
+		}
+
+});
+
+
+function onLocationFound(e) {
+	locError = e.message;
+    
+	if (marker != null){
+		marker.setLatLng(e.latlng);
+	}
+	else {
+		map.setView(e.latlng, 10);
+		marker = L.marker(e.latlng).addTo(map);
+	}
+	
+	
+}
+map.on('locationfound', onLocationFound);
+
+function onLocationError(e) {
+	locError = e.message;
+
+	$('#locatelabel').attr('title', locError)
+					 .tooltip('fixTitle')
+					 .tooltip('show');
+
+	setTimeout(function(){
+		
+		$('#locatelabel').tooltip('hide');
+	}, 4000);
+	
+	if (marker != null){
+		map.removeLayer(marker);
+		marker = null;
+	}
+}
+map.on('locationerror', onLocationError);
 
 var slownikWskaznikow = {				//przypisanie nazw dla zmiennych Danger Indexów
 	"LYME_DIS" : "Castor bean tick",
 	"HYALOMMA" : "Hyalomma marginatum tick",
 	"IXODES_R" : "Castor bean tick",
-	"AEDES_ALBO" : "Tiger mosquito",	
+	"AEDES_ALBO" : "Tiger mosquito",
 	"HOGWEED" : "Sosnowsky's hogweed",
-	"MACROVIPER" : "Milos viper",	
-	"V_XANTHINA" : "Rock viper",	
-	"V_AMMODYTE" : "Horned viper",	
-	"V_ASPIS" : "Asp viper",	
-	"V_BERUS" : "Common European viper",	
-	"V_LATASTEI" : "Latastes viper",	
-};	
+	"MACROVIPER" : "Milos viper",
+	"V_XANTHINA" : "Rock viper",
+	"V_AMMODYTE" : "Horned viper",
+	"V_ASPIS" : "Asp viper",
+	"V_BERUS" : "Common European viper",
+	"V_LATASTEI" : "Latastes viper",
+};
 
 var wsp;			//aktualnie wybrany checkbox
 var wspNazwa;		//nazwa wybranego Danger Indexu
@@ -57,26 +116,26 @@ $(document).ready(function(){
 	trigger : 'manual',
 	delay: 200,
 	animation: 'fadeIn',
-	placement: 'left',
-	title: 'Select Danger Index',
+	placement: 'bottom',
 	});
-    
+
 });
- 
+
 function init(){		//funkcja uruchamiana po kliknięciu przycisku w menu początkowym
 
 	if (typeof wsp == "undefined") {
-		$('[data-toggle="tooltip"]').tooltip('show');
+		$('#proceed').tooltip('show');
 		setTimeout(function(){
-		$('[data-toggle="tooltip"]').tooltip('hide');
-		}, 2000);		
-	} 
+		$('#proceed').tooltip('hide');
+		}, 2000);
+	}
 	else {
 		$('[data-toggle="tooltip"]').tooltip('destroy');
+
 		if (document.readyState === "complete") {		//menu zminimalizuje się po załadowaniu wszystkich danych
 			dim.className = 'hide';		//wyłączanie zaciemnienia mapki
 			jQuery('div.window').toggleClass('collapsed');	//minimalizowanie menu
-				
+
 //			document.getElementById("data").innerHTML = '<b>hover on region</b>';	//wyświetlany tekst w oknie z danymi po prawej
 			check(jQuery('input[name="dane1"]:checked').val());			//sprawdzanie aktualnie wybranego checkboxa i ładowanie geojsonów
 		}
@@ -88,28 +147,28 @@ function check(radioValue) {
 
 	wspNazwa = slownikWskaznikow[radioValue];
 	wsp = radioValue;
- 
+
 	if (menu.className == 'window collapsed') {	//działa dopiero po zminimalizowaniu menu (po naciśnięciu przycisku)
-	
+
 		if (typeof regionPL != "undefined") {
 			map.removeLayer(regionPL);
-		}	
+		}
 		if (typeof regionCZ != "undefined") {
 			map.removeLayer(regionCZ);
-		}				
-	
+		}
+
 		if (radioValue == 'LYME_DIS' ){
-			
-			var plik = "opisy/lymeDiseasePolandDesc.html";
-			jQuery.get(plik, function(opis) {
-				document.getElementById("info").innerHTML = opis;
-			})
-		
-			Poland.addTo(map);	
+
+			// var plik = "opisy/lymeDiseasePolandDesc.html";
+			// jQuery.get(plik, function(opis) {
+				// document.getElementById("info").innerHTML = "";
+			// })
+
+			Poland.addTo(map);
 			group.addLayer(Poland);
 			Czech.addTo(map);
 			group.addLayer(Czech);
-		
+
 			WojPL.eachLayer(function (layer) {
 				layer.setStyle(styleLymePL);
 			});
@@ -124,50 +183,50 @@ function check(radioValue) {
 				map.addLayer(clickRegion);
 				Poland.resetStyle(clickRegion);
 				Czech.resetStyle(clickRegion);
-			}			
+			}
 		}
-	
+
 		if (radioValue == 'HYALOMMA' ){
 			document.getElementById("info").innerHTML = "";
-					
+
 			Europe.addTo(map);
 			group.addLayer(Europe);
-			
-			map.removeLayer(Poland);		
+
+			map.removeLayer(Poland);
 			map.removeLayer(Czech);
 			group.removeLayer(Poland);
 			group.removeLayer(Czech);
-			
-			Europe.setStyle(styleEUHYA);	
+
+			Europe.setStyle(styleEUHYA);
 		}
 
 		else if (radioValue == 'IXODES_R' ){
 			document.getElementById("info").innerHTML = "";
-					
+
 			Europe.addTo(map);
 			group.addLayer(Europe);
-			
 
-			map.removeLayer(Poland);			
-			map.removeLayer(Czech);		
+
+			map.removeLayer(Poland);
+			map.removeLayer(Czech);
 			group.removeLayer(Poland);
 			group.removeLayer(Czech);
-			
-			Europe.setStyle(styleEUIXO);	
+
+			Europe.setStyle(styleEUIXO);
 		}
-	
+
 		else if (radioValue == 'AEDES_ALBO' ) {
 			document.getElementById("info").innerHTML = "";
 
 			Europe.addTo(map);
 			group.addLayer(Europe);
 
-			map.removeLayer(Poland);			
+			map.removeLayer(Poland);
 			map.removeLayer(Czech);
 			group.removeLayer(Poland);
 			group.removeLayer(Czech);
-			
-			Europe.setStyle(styleEUAED);	
+
+			Europe.setStyle(styleEUAED);
 		}
 		else if (radioValue == 'MACROVIPER' ) {
 			document.getElementById("info").innerHTML = "";
@@ -175,25 +234,25 @@ function check(radioValue) {
 			Europe.addTo(map);
 			group.addLayer(Europe);
 
-			map.removeLayer(Poland);			
+			map.removeLayer(Poland);
 			map.removeLayer(Czech);
 			group.removeLayer(Poland);
 			group.removeLayer(Czech);
-			
-			Europe.setStyle(styleEUMV);	
-		}		
+
+			Europe.setStyle(styleEUMV);
+		}
 		else if (radioValue == 'V_XANTHINA' ) {
 			document.getElementById("info").innerHTML = "";
 
 			Europe.addTo(map);
 			group.addLayer(Europe);
 
-			map.removeLayer(Poland);			
+			map.removeLayer(Poland);
 			map.removeLayer(Czech);
 			group.removeLayer(Poland);
 			group.removeLayer(Czech);
-			
-			Europe.setStyle(styleEUVX);	
+
+			Europe.setStyle(styleEUVX);
 		}
 		else if (radioValue == 'V_AMMODYTE' ) {
 			document.getElementById("info").innerHTML = "";
@@ -201,12 +260,12 @@ function check(radioValue) {
 			Europe.addTo(map);
 			group.addLayer(Europe);
 
-			map.removeLayer(Poland);			
+			map.removeLayer(Poland);
 			map.removeLayer(Czech);
 			group.removeLayer(Poland);
 			group.removeLayer(Czech);
-			
-			Europe.setStyle(styleEUVAM);	
+
+			Europe.setStyle(styleEUVAM);
 		}
 		else if (radioValue == 'V_ASPIS' ) {
 			document.getElementById("info").innerHTML = "";
@@ -214,12 +273,12 @@ function check(radioValue) {
 			Europe.addTo(map);
 			group.addLayer(Europe);
 
-			map.removeLayer(Poland);			
+			map.removeLayer(Poland);
 			map.removeLayer(Czech);
 			group.removeLayer(Poland);
 			group.removeLayer(Czech);
-			
-			Europe.setStyle(styleEUVAS);	
+
+			Europe.setStyle(styleEUVAS);
 		}
 		else if (radioValue == 'V_BERUS' ) {
 			document.getElementById("info").innerHTML = "";
@@ -227,12 +286,12 @@ function check(radioValue) {
 			Europe.addTo(map);
 			group.addLayer(Europe);
 
-			map.removeLayer(Poland);			
+			map.removeLayer(Poland);
 			map.removeLayer(Czech);
 			group.removeLayer(Poland);
 			group.removeLayer(Czech);
-			
-			Europe.setStyle(styleEUVB);	
+
+			Europe.setStyle(styleEUVB);
 		}
 		else if (radioValue == 'V_LATASTEI' ) {
 			document.getElementById("info").innerHTML = "";
@@ -240,44 +299,44 @@ function check(radioValue) {
 			Europe.addTo(map);
 			group.addLayer(Europe);
 
-			map.removeLayer(Poland);			
+			map.removeLayer(Poland);
 			map.removeLayer(Czech);
 			group.removeLayer(Poland);
 			group.removeLayer(Czech);
-			
-			Europe.setStyle(styleEUVL);	
-		}		
-		
+
+			Europe.setStyle(styleEUVL);
+		}
+
 		else {
-			map.removeLayer(Europe);		
+			map.removeLayer(Europe);
 			group.removeLayer(Europe);
 		}
-	
+
 		if (radioValue == 'HOGWEED' ) {
 			document.getElementById("info").innerHTML = "";
 
-			Poland.addTo(map);	
+			Poland.addTo(map);
 			group.addLayer(Poland);
-				
+
 			Poland.setStyle(noData);
-			
+
 			if (map.hasLayer(hogweedPoland) != true) {
 				hogweedPoland.addTo(map);
-			}	
+			}
 
-			map.removeLayer(Czech);		
+			map.removeLayer(Czech);
 			group.removeLayer(Czech);
-		
+
 		} else {
 			if (map.hasLayer(hogweedPoland)){
 				map.removeLayer(hogweedPoland);
-			}			
+			}
 		}
-		
-		
+
+
 		if (jQuery.isEmptyObject(group.getLayers()) == false){
 			map.fitBounds(group.getBounds());
-		}	
+		}
 
 		legend.update();
 
@@ -296,7 +355,7 @@ info.onAdd = function (map) {
 info.update = function (props) {
 
 			info._div.innerHTML = document.getElementById("data").innerHTML
-		
+
 };
 info.addTo(map);
 
@@ -323,9 +382,9 @@ legend.update = function () {
 		grades = [0, 751, -1];
 		labels = ["<b>safe zone</b>", "<b>danger zone</b></br>", "<b>no data</b>"];
 	}
-		
+
 	this.div.innerHTML = '';
-	
+
 	if (typeof getColorPL != "undefined") {
 		this.div.innerHTML = '';
 		for (var i = 0; i < grades.length; i++) {
@@ -338,9 +397,9 @@ legend.addTo(map);
 
 map.on('zoomstart', function() {
 	map.removeLayer(labels);
-});		
+});
 
 map.on('zoomend', function() {
 	labels.addTo(map);
-	
+
 });
